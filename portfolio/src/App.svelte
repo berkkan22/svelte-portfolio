e6dee80f377033875325d7cd62b2324e2e7cf215
Berkkan <berkkan.katirci@studium.uni-hamburg.de>
2024-04-03 18:28:27 +0200

    Fix wrong font in some browsers

--- portfolio/src/App.svelte
+++ portfolio/src/App.svelte
@@ -7,6 +7,12 @@
   import "./app.css";
 </script>
 
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet" />
+</svelte:head>
+
 <main class={$themeColor}>
   <Navigation />
 
